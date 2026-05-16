d9e7d4eea57c52fb1b4daf4c6da327a7c8f5ef8f
themanprince <princeadigwe29@gmail.com>
2026-05-15 19:25:48 -0700

    UnitService.create_conversion_rule()

--- service/UnitService.py
+++ service/UnitService.py
@@ -1,11 +1,8 @@
 from decimal import Decimal
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from db import ProductUnitConversion
-
-
-class UnitConversionError(Exception):
-    pass
+from db import ProductUnitConversion, Product, Unit
+from exceptions import UnitConversionError, CreateConversionRuleError
 
 
 class UnitService:
@@ -31,3 +28,27 @@ class UnitService:
         base_quantity = quantity * Decimal(conversion.multiplier_to_base)
 
         return base_quantity
+    
+
+    def create_conversion_rule(self, product_id:int, unit_id:int, multiplier_to_base:Decimal, override_existing_rule = False):
+        existing_rule = self.session.query(ProductUnitConversion).filter(ProductUnitConversion.product_id == product_id, ProductUnitConversion.unit_id == unit_id).first()
+
+        if existing_rule and (not override_existing_rule):
+            raise CreateConversionRuleError(f"Conversion rule already exists with product_id={product_id}, unit_id={unit_id}. Try setting override_existing_rule param to True if you wish to override an existing rule")
+        
+        product = self.session.query(Product).filter(Product.id == product_id).first()
+        unit = self.session.query(Unit).filter(Unit.id == unit_id).first()
+
+        if (not product) or (not unit):
+            raise CreateConversionRuleError(f"Either of these doesn't exist: Product with id={product_id}, unit with id={unit_id}")
+        
+        conversion_rule = ProductUnitConversion(
+            product_id = product_id,
+            unit_id = unit_id,
+            multiplier_to_base = multiplier_to_base
+        )
+
+        self.session.add(conversion_rule)
+        self.session.flush()
+
+        return conversion_rule
