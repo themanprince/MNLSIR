53fdfb06efc2e960286904e626f1b388ddc7db3c
themanprince <princeadigwe29@gmail.com>
2026-05-16 06:38:55 -0700

    extracted certain helper functions for re-usability purposes

--- test/test_unit_service.py
+++ test/test_unit_service.py
@@ -1,59 +1,10 @@
 import pytest
 from sqlalchemy.orm import Session
 from .conftest import db_session
+from .helpers import seed_test_unit, seed_test_product, seed_conversion_rule, setup_achi, setup_yoghurt_plain
 from service.UnitService import UnitService
-from db import Product, Unit, ProductUnitConversion
 from exceptions import UnitConversionError
 from decimal import Decimal
-
-
-def seed_test_unit(session: Session, name:str, symbol:str):
-    unit = Unit(name = name, symbol = symbol)
-    session.add(unit)
-    session.commit()
-    session.refresh(unit)
-    return unit
-
-
-def seed_test_product(session:Session, name:str, sku:str, base_unit_id:int):
-    product = Product(name=name, sku=sku, base_unit_id=base_unit_id)
-    session.add(product)
-    session.commit()
-    session.refresh(product)
-    return product
-
-def seed_conversion_rule(session: Session, product_id: int, unit_id:int, multiplier_to_base: Decimal):
-    rule = ProductUnitConversion(
-        product_id = product_id,
-        unit_id = unit_id,
-        multiplier_to_base = multiplier_to_base
-    )
-    session.add(rule)
-    session.commit()
-    session.refresh(rule)
-    return rule
-
-
-#next, I'll be putting functions for easily setting up test products, units and conversion rules
-def setup_yoghurt_plain(session: Session):
-    base_unit = seed_test_unit(session = session, name="piece", symbol="pcs")
-    other_unit = seed_test_unit(session = session, name="carton", symbol="ctn")
-    product = seed_test_product(session = session, name="Yoghurt Plain", sku="yoghurt-plain", base_unit_id=base_unit.id)
-    
-    multiplier_to_base = Decimal("10")
-    seed_conversion_rule(session = session, product_id = product.id, unit_id=other_unit.id, multiplier_to_base=multiplier_to_base)
-
-    return base_unit, other_unit, product, multiplier_to_base
-
-def setup_achi(session: Session):
-    base_unit = seed_test_unit(session = session, name="kilogram", symbol="kg")
-    other_unit = seed_test_unit(session = session, name="basket", symbol="bskt")
-    product = seed_test_product(session = session, name="Achi", sku="achi", base_unit_id=base_unit.id)
-    
-    multiplier_to_base = Decimal("2.5")
-    seed_conversion_rule(session = session, product_id = product.id, unit_id=other_unit.id, multiplier_to_base=multiplier_to_base)
-
-    return base_unit, other_unit, product, multiplier_to_base
 
 
 
