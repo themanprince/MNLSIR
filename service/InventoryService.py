f2e137e873c6617b7e570258667759813d651678
themanprince <princeadigwe29@gmail.com>
2026-05-19 22:27:03 -0700

    implemented InventoryService.submit_stocktake()

--- service/InventoryService.py
+++ service/InventoryService.py
@@ -1,10 +1,12 @@
 from sqlalchemy.orm import Session
-from db import Document, DocumentType, DocumentLine, StockMovement, MovementType
+from sqlalchemy import select
+from db import Document, DocumentType, DocumentLine, StockMovement, MovementType, StockBalance, InterventionLog, ActionType
 from schema.ReceiveStockRequest import ReceiveStockRequest
 from service.UnitService import UnitService
 from service.StockService import StockService
 from exceptions import ReceiveStockError
 from datetime import date
+from decimal import Decimal
 
 
 class InventoryService:
@@ -79,3 +81,58 @@ class InventoryService:
                 )
 
             return document
+    
+
+    def submit_stocktake(self, store_id: int, product_id: int, target_quantity: Decimal, operator_name: str, remarks: str, stocktake_date:date = date.today()):
+        # this handles some scenarios as follows
+        # 1. the scenario where store keeper needs to update digital stock balance of a product to align with its physical stock balance, in cases of observed but inexplainable discrepancies
+        # 2. fresh inventory taking
+        transaction_context = ( # if a transaction is already started, use a nested savepoint transaction. Otherwise, start a top-level transaction
+            self.session.begin_nested()
+            if self.session.in_transaction()
+            else self.session.begin()
+        )
+        with transaction_context:
+            lock_statement = ( #so nobody updates StockBalance while I'm still working with it
+                select(StockBalance)
+                .where(StockBalance.store_id == store_id, StockBalance.product_id == product_id)
+                .with_for_update()
+            )
+            self.session.execute(lock_statement)
+
+            current_balance_record = self.session.query(StockBalance).filter(StockBalance.store_id == store_id, StockBalance.product_id == product_id).first()
+            current_quantity = current_balance_record.quantity if current_balance_record else Decimal("0")
+
+            action_type = ActionType.INITIAL_STOCK_TAKE if current_balance_record is None else ActionType.BALANCE_OVERWRITE_RECONCILE
+
+            stock_movement = StockMovement(
+                store_id = store_id,
+                product_id = product_id,
+                movement_type = MovementType.ADJUST,
+                quantity_delta = Decimal("0"), # this is an adjustment stock movement.. the stock balance should be changed to the set target_quantity, and not be calculated based on some quantity_delta
+                target_quantity = target_quantity,
+                movement_date = stocktake_date #explicitly passed, as guard against delayed submissions
+            )
+
+            self.session.add(stock_movement)
+            self.session.flush()
+
+            #logging the action into out audit trail
+            intervention_log = InterventionLog(
+                store_id = store_id,
+                product_id = product_id,
+                action_type = action_type,
+                concerned_movement_id = stock_movement.id,
+                old_value_snapshot = current_quantity,
+                new_value_snapshot = target_quantity,
+                changed_by = operator_name,
+                remarks = remarks
+            )
+
+            self.session.add(intervention_log)
+            self.session.flush()
+
+            self.stock_service.recalculate(store_id=store_id, product_id = product_id, from_movement_date = stocktake_date)
+    
+
+    
