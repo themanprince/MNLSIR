b2b5746134d6d948ec0a0f917fe8d9d6637f968b
themanprince <princeadigwe29@gmail.com>
2026-05-20 20:08:59 -0700

    updated StockService.recalculate() to handle StockMovements with movement_type=MovementType.STOCKTAKE.. ALSO implemented StockService.update_historical_stockmovement()... ALSO implemented and used new exception UpdateStockMovementError

--- exceptions.py
+++ exceptions.py
@@ -6,3 +6,6 @@ class CreateConversionRuleError(Exception):
 
 class ReceiveStockError(Exception):
     pass
+
+class UpdateStockMovementError(Exception):
+    pass
--- service/StockService.py
+++ service/StockService.py
@@ -1,8 +1,9 @@
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from db import StockMovement, StockBalance, MovementType
+from db import StockMovement, StockBalance, MovementType, InterventionLog, ActionType
 from datetime import date
 from decimal import Decimal
+from exceptions import UpdateStockMovementError
 
 
 class StockService:
@@ -60,7 +61,7 @@ class StockService:
         )
 
         for movement in movements:
-            if movement.movement_type == MovementType.ADJUST:
+            if movement.movement_type == MovementType.STOCKTAKE:
                 running_balance = movement.target_quantity #reset running balance to the value adjusted to
             else:
                 running_balance += movement.quantity_delta #regular issue/receipt
@@ -87,3 +88,45 @@ class StockService:
             self.session.flush()
         else:
             stock_balance.quantity = running_balance
+    
+
+    def update_historical_stockmovement(self, movement_id:int, new_quantity_delta:Decimal, operator_name: str, remarks: str):
+        # this allows store keepers to update a previously recorded stock movement..
+        # the rurnning balances of stock movements following that one will be recalculated
+        transaction_context = ( # if a transaction is already started, use a nested savepoint transaction. Otherwise, start a top-level transaction
+            self.session.begin_nested()
+            if self.session.in_transaction()
+            else self.session.begin()
+        )
+        with transaction_context:
+            lock_statement = (
+                select(StockMovement)
+                .where(StockMovement.id == movement_id)
+                .with_for_update()
+            )
+            self.session.execute(lock_statement)
+
+            stock_movement = self.session.query(StockMovement).filter_by(id = movement_id).first()
+            if not stock_movement:
+                raise UpdateStockMovementError("Target stock movement record not found")
+            
+            old_quantity_delta = stock_movement.quantity_delta
+
+            stock_movement.quantity_delta = new_quantity_delta
+
+            intervention_log = InterventionLog(
+                store_id = stock_movement.store_id,
+                product_id = stock_movement.product_id,
+                source_action_type = ActionType.IN_PLACE_EDIT,
+                concerned_movement_id = stock_movement.id,
+                old_value_snapshot = old_quantity_delta,
+                new_value_snapshot = new_quantity_delta,
+                changed_by = operator_name,
+                remarks = remarks
+            )
+
+            self.session.add(intervention_log)
+            self.session.flush()
+
+            #StockBalance will be updated in recalculate() method
+            self.recalculate(store_id=stock_movement.store_id, product_id=stock_movement.product_id, from_movement_date=stock_movement.movement_date)
