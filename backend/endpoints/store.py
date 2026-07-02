16f4a4466622f61dca78f971b1a2adaaa938eea8
themanprince <princeadigwe29@gmail.com>
2026-07-01 22:11:15 -0700

    modified endpoints/store.py after discovering error on test run

--- backend/endpoints/store.py
+++ backend/endpoints/store.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, Depends, Body
+from fastapi import APIRouter, Depends, Body, HTTPException
 from db import get_session
 from service.repo.StoreRepo import StoreRepo
 
+
 StoreRouter = APIRouter(prefix="/store")
 
-@StoreRouter.post("/", status_code=201)
-async def create_store(store_name = Depends(Body(embed=True)), session = Depends(get_session)):
-    store = StoreRepo.create_new_store(store_name = store_name, session = session)
-    return store.id
+@StoreRouter.post("/{store_name}", status_code=201)
+async def create_store(store_name, session = Depends(get_session)):
+    try:
+        store = StoreRepo.create_new_store(store_name = store_name, session = session)
+        return {"store_id": store.id}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
