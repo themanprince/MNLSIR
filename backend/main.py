99989559ea5ce7d95f9e57b90637a655813f85b1
themanprince <princeadigwe29@gmail.com>
2026-07-14 15:25:15 -0700

    UnitRouter and CORS

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,13 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from db import Base, engine
 from endpoints.ledger import LedgerRouter
 from endpoints.store import StoreRouter
 from endpoints.inventory import InventoryRouter
 from endpoints.product import ProductRouter
+from endpoints.unit import UnitRouter
+from CONSTANTS import FRONTEND_URL
 
 
 @asynccontextmanager
@@ -14,10 +17,18 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan = lifespan)
 
+app.add_middleware(
+      CORSMiddleware,
+      allow_origins=[FRONTEND_URL],
+      allow_methods=["*"],
+      allow_headers=["*"]
+)
+
 app.include_router(LedgerRouter)
 app.include_router(StoreRouter)
 app.include_router(InventoryRouter)
 app.include_router(ProductRouter)
+app.include_router(UnitRouter)
 
 if __name__ == "__main__":
 	import uvicorn
