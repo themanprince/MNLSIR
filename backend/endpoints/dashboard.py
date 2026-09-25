f7889633dd6c02d88d27dd0d36e703d6b8acdf2c
Prince Adigwe <princeadigwe29@gmail.com>
2026-09-25 21:10:19 +0000

    attempt at fixing dashboard

--- backend/endpoints/dashboard.py
+++ backend/endpoints/dashboard.py
@@ -2,6 +2,13 @@ from sqladmin import BaseView, expose
 
 class DashboardView(BaseView):
     name = "Home"
+    
+    def is_accessible(self, request: Request) -> bool:
+        return is_logged_in(request)
+
+    def is_visible(self, request: Request) -> bool:
+        return is_logged_in(request)
+
 
     @expose("/", methods=["GET", "HEAD"])
     async def index_page(self, request):
