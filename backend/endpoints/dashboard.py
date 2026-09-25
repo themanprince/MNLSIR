424723a1610b149bcc6206075ae48f1460cb0621
Prince Adigwe <princeadigwe29@gmail.com>
2026-09-25 05:55:01 +0000

    avoid name collision with sqladmin templates

--- backend/endpoints/dashboard.py
+++ backend/endpoints/dashboard.py
@@ -3,12 +3,11 @@ from sqladmin import BaseView, expose
 class DashboardView(BaseView):
     is_index = True
     name = "Home"
-    icon = "fa fa-home"
 
     @expose("/", methods=["GET"])
     async def index_page(self, request):
         
         return await self.templates.TemplateResponse(
             request, 
-            "index.html",
+            "dashboard.html",
         )
